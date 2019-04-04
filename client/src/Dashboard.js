ba66135078a07a313ce6b17a03e66eeac622c3de
Mike Natale <mike.natale@bhvr.com>
2019-04-04 10:29:24 -0400

    cleans up repo, adds blank dashboard component

---
+++ client/src/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                beep boop
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
