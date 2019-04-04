4dcf25c7b540ad8c13327312f1c72bd5339a3a53
Rémy Jeancolas <remy.jeancolas@bhvr.com>
2019-04-04 16:05:36 -0400

    chore: refactor app (2)

--- server/src/Application.ts
+++ server/src/Application.ts
@@ -33,10 +33,12 @@ export class Application {
 
     this.eventProcessor.on(EventType.IssueUpdated, (issue) => {
       this.store.save(issue);
+      this.webSocket.emitEvent('scoresUpdated', this.computeScores());
     });
 
     this.eventProcessor.on(EventType.IssueDeleted, (issue) => {
       this.store.deleteById(issue.id);
+      this.webSocket.emitEvent('scoresUpdated', this.computeScores());
     });
   }
 
@@ -45,48 +47,7 @@ export class Application {
     this.app.use(bodyParser.json());
 
     this.app.get('/', (req, res, next) => {
-  
-      //formula to calculate score
-      const calcScore = (issue: Issue): number => {
-        return 10000 / issue.priority;
-      };
-    
-      const issues = this.store.getAll();
-      console.log(issues);
-      let result: Ranking = {
-        totalIssues: 0,
-        totalScores: 0,
-        completedIssues: 0,
-        completedScores: 0,
-        topUsers: []
-      };
-      for(const i of issues) {
-        const score = calcScore(i);
-        if (i.user) {
-          const user = result.topUsers.find(x => x.id === i.user.id);
-          if (!user) {
-            const configInfo = config.get<any>('users').find(x => x.jiraId === i.user.id);
-            result.topUsers.push({
-              id: i.user.id,
-              name: configInfo ? configInfo.name : config.get<any>('defaultUser').name,
-              avatar: configInfo ? configInfo.avatar : config.get<any>('defaultUser').avatar,
-              score: i.status === IssueStatus.Done ?  score : 0
-            })
-          }
-          else {
-            user.score += score;
-          }
-        }
-        result.totalIssues++;
-        result.totalScores += score;
-        if (i.status === IssueStatus.Done) {
-          result.completedIssues++;
-          result.completedScores += score;
-        }
-      }
-      //return only the top users
-      result.topUsers = result.topUsers.sort((a, b) => b.score - a.score).splice(0, config.get<number>('displayLimit'))
-      res.status(200).json(result);
+      res.status(200).json(this.computeScores());
     });
 
     this.app.post('/jira/event', (req, res, next) => {
@@ -104,6 +65,52 @@ export class Application {
     this.webSocket.server.on('connection', (socket) => {
       this.webSocket.handleNewConnection(socket);
     });
+  }
+
+  private computeScores(): Ranking {
+    //formula to calculate score
+    const calcScore = (issue: Issue): number => {
+      return 10000 / issue.priority;
+    };
+  
+    const issues = this.store.getAll();
+    console.log(issues);
+    let result: Ranking = {
+      totalIssues: 0,
+      totalScores: 0,
+      completedIssues: 0,
+      completedScores: 0,
+      topUsers: []
+    };
+    for(const i of issues) {
+      const score = calcScore(i);
+      if (i.user) {
+        const user = result.topUsers.find(x => x.id === i.user.id);
+        if (!user) {
+          const configInfo = config.get<any>('users').find(x => x.jiraId === i.user.id);
+          result.topUsers.push({
+            id: i.user.id,
+            name: configInfo ? configInfo.name : config.get<any>('defaultUser').name,
+            avatar: configInfo ? configInfo.avatar : config.get<any>('defaultUser').avatar,
+            score: i.status === IssueStatus.Done ?  score : 0
+          })
+        }
+        else {
+          user.score += score;
+        }
+      }
+      result.totalIssues++;
+      result.totalScores += score;
+      if (i.status === IssueStatus.Done) {
+        result.completedIssues++;
+        result.completedScores += score;
+      }
+    }
+
+    //return only the top users
+    result.topUsers = result.topUsers.sort((a, b) => b.score - a.score).splice(0, config.get<number>('displayLimit'));
+
+    return result;
   }
 
   public start(): Promise<void> {
