9a39bf31a9d7112ca4fbf0862d9864bfef73a50f
Rémy Jeancolas <remy.jeancolas@bhvr.com>
2019-04-04 17:32:49 -0400

    feat: send events from server to client using websockets

---
+++ client/src/middlewares/WebSocket.js
@@ -0,0 +1,45 @@
+import { RECEIVE_DATA } from '../actions/Application';
+
+class WebSocket {
+  static store;
+  static socket;
+  
+  static getSocket() {
+    if (!this.socket) {
+      // Create socket
+      /* global io */
+      this.socket = io('http://localhost:8081');
+      
+      // Automatically reconnect on disconnection
+      this.socket.on('disconnect', (reason) => {
+        if (reason === 'io server disconnect') {
+          this.socket.connect();
+        }
+      });
+            
+      // Listen to all events on socket and dispatch them as is
+      this.socket.on('scoresUpdated', (data) => {
+        if (this.store) {
+          this.store.dispatch({type: RECEIVE_DATA, data});
+        }
+      });
+    }
+    
+    return this.socket;
+  }
+  
+  static middleware = (store) => {
+    // Save a reference to current store
+    WebSocket.store = store;
+    
+    // Initialize socket
+    WebSocket.getSocket();
+    
+    return (next) => async action => {     
+      return next(action);
+    };
+  }
+}
+
+// tslint:disable-next-line:export-name
+export const WebSocketMiddleware = WebSocket.middleware;
